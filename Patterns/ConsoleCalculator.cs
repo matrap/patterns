a6431d5f4dc763e22139a56d020537adc0d9715b
Mateusz Rapacz <mateusz.rapacz@gmail.com>
2016-12-03 00:14:32 +0100

    Decorator pattern

---
+++ Patterns/ConsoleCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class ConsoleCalculator : IConsoleCalculator
+    {
+        public ICalculator Calculator { get; private set; }
+
+        public ConsoleCalculator(ICalculator calculator)
+        {
+            Calculator = calculator;
+        }
+
+        public int Add(int a, int b)
+        {
+            int sum = Calculator.Add(a, b);
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+            return sum;
+        }
+    }
+}
--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Decorator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace Patterns
     {
         static void Main(string[] args)
         {
+            ICalculator standardCalc = new StandardCalculator();
+            ICalculator consoleCalc = new ConsoleCalculator(standardCalc);
+            int a = 5;
+            int b = 7;
+            Console.WriteLine("Output from ConsoleCalculator:");
+            int consoleSum = consoleCalc.Add(5, 7);
+            Console.WriteLine("Output rewrite from result of method: {0}", consoleSum);
         }
     }
 }
